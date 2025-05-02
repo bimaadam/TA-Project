124423407cea3f12f05532f76e191e00c79b71e2
bimaadam <rinbim.dev@gmail.com>
2025-05-02 21:10:25 +0700

    fix view table

--- src/app/(admin)/project-report/page.tsx
+++ src/app/(admin)/project-report/page.tsx
@@ -13,13 +13,9 @@ export default function ProjectReportPage() {
     return (
         <div>
             <PageBreadcrumb pageTitle="Project Report" />
-            <div className="min-h-screen rounded-2xl border border-gray-200 bg-white px-5 py-7 dark:border-gray-800 dark:bg-white/[0.03] xl:px-10 xl:py-12">
-                <div className="mx-auto w-full max-w-[630px] text-center">
-                    <ComponentCard title="Laporan Proyek" >
-                        <ProjectReportTable />
-                    </ComponentCard>
-                </div>
-            </div>
+            <ComponentCard title="Laporan Proyek" >
+                <ProjectReportTable />
+            </ComponentCard>
         </div>
     );
 }
