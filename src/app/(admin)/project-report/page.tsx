b1be10c7d880489eb5133018c08c088901b585b1
bimaadam <rinbim.dev@gmail.com>
2025-05-02 21:06:31 +0700

    fix

--- src/app/(admin)/project-report/page.tsx
+++ src/app/(admin)/project-report/page.tsx
@@ -1,6 +1,6 @@
 import ComponentCard from "@/components/common/ComponentCard";
 import PageBreadcrumb from "@/components/common/PageBreadCrumb";
-import BasicTableOne from "@/components/tables/BasicTableOne";
+import ProjectReportTable from "@/components/tables/ProjectReport";
 import { Metadata } from "next";
 import React from "react";
 
@@ -16,7 +16,7 @@ export default function ProjectReportPage() {
             <div className="min-h-screen rounded-2xl border border-gray-200 bg-white px-5 py-7 dark:border-gray-800 dark:bg-white/[0.03] xl:px-10 xl:py-12">
                 <div className="mx-auto w-full max-w-[630px] text-center">
                     <ComponentCard title="Laporan Proyek" >
-                        <BasicTableOne />
+                        <ProjectReportTable />
                     </ComponentCard>
                 </div>
             </div>
