219cbefc7c827d24487a821a3e892457df3956fe
bimaadam <rinbim.dev@gmail.com>
2025-05-04 10:30:33 +0700

    fix callback

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -27,7 +27,7 @@ export const authOptions: NextAuthOptions = {
               lastName: credentials.lname,
               email: credentials.email,
               password: hashedPassword,
-              role: 'user' // Default role
+              role: 'ADMIN' // Default role
             }
           });
           return {
@@ -80,10 +80,7 @@ export const authOptions: NextAuthOptions = {
     async jwt({ token, user }) {
       if (user) {
         token.id = user.id;
-        token.role = user.role;
         token.name = user.name;
-        token.firstName = user.firstName;
-        token.lastName = user.lastName;
         token.email = user.email;
       }
       return token;
@@ -91,11 +88,7 @@ export const authOptions: NextAuthOptions = {
     async session({ session, token }) {
       if (token) {
         session.user = {
-          id: token.id,
-          role: token.role,
           name: token.name,
-          firstName: token.firstName,
-          lastName: token.lastName,
           email: token.email
         };
       }
