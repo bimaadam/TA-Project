9676c9701e050b34d313579ba6001827bc1486d1
bimaadam <rinbim.dev@gmail.com>
2025-05-05 19:57:17 +0700

    add page data invoice

---
+++ src/components/data-invoice/Invoice.tsx
@@ -0,0 +1,11 @@
+import Badge from "../ui/badge/Badge"
+
+const Invoice = () => {
+    return (
+        <div>
+            <Badge children={"Data Invoice"} />
+            <h1>Ini Adalah data invoice</h1></div>
+    )
+}
+
+export default Invoice()
