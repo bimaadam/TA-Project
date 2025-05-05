c1c089ccdc9ca09f0517b08a459d328a31eb719d
bimaadam <rinbim.dev@gmail.com>
2025-05-05 20:04:36 +0700

    fixed solved

--- src/components/data-invoice/Invoice.tsx
+++ src/components/data-invoice/Invoice.tsx
@@ -1,9 +1,7 @@
-import Badge from "../ui/badge/Badge"
 
 const Invoice = () => {
     return (
         <div>
-            <Badge children={"Data Invoice"} />
             <h1>Ini Adalah data invoice</h1></div>
     )
 }
