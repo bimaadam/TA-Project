4f15a37b2f73a944e55cf980576c599ac086dc06
bimaadam <rinbim.dev@gmail.com>
2025-05-06 19:02:55 +0700

    fix page on component ssr

--- src/app/master-data-klien/page.tsx
+++ src/app/master-data-klien/page.tsx
@@ -1,12 +1,13 @@
 import ComponentCard from "@/components/common/ComponentCard"
 import PageBreadcrumb from "@/components/common/PageBreadCrumb"
+import MAsterDataClient from "@/components/master-data/Master"
 
 export default function MasterDataKlienPage() {
     return (
         <div>
             <PageBreadcrumb pageTitle="Master Data Client" />
             <ComponentCard title="Data Client">
-                <h1>Ini adalah page master data klien</h1>
+                <MAsterDataClient />
             </ComponentCard>
         </div>
     )
---
+++ src/components/master-data/Master.tsx
@@ -0,0 +1,7 @@
+export default function MAsterDataClient() {
+    return (
+        <div>
+            <h1>INI ADALAH PAGE MASTER DATA CLIENT</h1>
+        </div>
+    )
+}
