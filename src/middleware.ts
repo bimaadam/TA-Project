30bca506c6b35be3da51f5726fb77a46d80eb49f
bimaadam <rinbim.dev@gmail.com>
2025-05-24 10:37:42 +0700

    fix middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,19 +1,7 @@
 import { NextResponse } from 'next/server';
-import type { NextRequest } from 'next/server';
 
-export function middleware(req: NextRequest) {
-  const token = localStorage
-
-  const protectedPaths = ['/', '/dashboard'];
-  const isProtected = protectedPaths.some((path) =>
-    req.nextUrl.pathname.startsWith(path)
-  );
-
-  if (!token && isProtected) {
-    return NextResponse.redirect(new URL('/signin', req.url));
-  }
-
-  return NextResponse.next();
+export function middleware() {
+  return NextResponse.next(); // Biarkan frontend handle auth
 }
 
 export const config = {
