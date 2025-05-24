968f5ae7146069fe4a436b34d749a167d7c1f9cc
bimaadam <rinbim.dev@gmail.com>
2025-05-24 10:57:05 +0700

    client component fix

--- src/app/(admin)/layout.tsx
+++ src/app/(admin)/layout.tsx
@@ -4,22 +4,22 @@ import { useSidebar } from "@/context/SidebarContext";
 import AppHeader from "@/layout/AppHeader";
 import AppSidebar from "@/layout/AppSidebar";
 import Backdrop from "@/layout/Backdrop";
-import router from "next/router";
+import { useRouter } from "next/navigation";
 import React, { useEffect } from "react";
-
 
 export default function AdminLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const router = useRouter();
+
   useEffect(() => {
     const token = localStorage.getItem('sessionToken');
     if (!token) router.push('/signin');
-  }, []);
+  }, [router]);
 
   const { isExpanded, isHovered, isMobileOpen } = useSidebar();
-  // Dynamic class for main content margin based on sidebar state
   const mainContentMargin = isMobileOpen
     ? "ml-0"
     : isExpanded || isHovered
@@ -28,16 +28,12 @@ export default function AdminLayout({
 
   return (
     <div className="min-h-screen xl:flex">
-      {/* Sidebar and Backdrop */}
       <AppSidebar />
       <Backdrop />
-      {/* Main Content Area */}
       <div
         className={`flex-1 transition-all  duration-300 ease-in-out ${mainContentMargin}`}
       >
-        {/* Header */}
         <AppHeader />
-        {/* Page Content */}
         <div className="p-4 mx-auto max-w-(--breakpoint-2xl) md:p-6">{children}</div>
       </div>
     </div>
