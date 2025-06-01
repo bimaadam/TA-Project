23cef9aefc3454ae555c56317e0e6d07f65f9835
bimaadam <rinbim.dev@gmail.com>
2025-06-01 23:09:39 +0700

    add page order

---
+++ src/app/(admin)/(transaction)/finance/order/page.tsx
@@ -0,0 +1,5 @@
+import NewOrder from "@/components/new-order"
+
+export default function OrderPage() {
+    return <NewOrder />
+}
---
+++ src/components/new-order/index.tsx
@@ -0,0 +1,17 @@
+
+import DefaultInputs from "./Input-Order";
+
+
+const NewOrder = () => {
+    return (
+        <div className="h-full">
+            <div className="flex-1 w-full ">
+                <div className="text-white bg-auto">
+                    <DefaultInputs />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default NewOrder;
