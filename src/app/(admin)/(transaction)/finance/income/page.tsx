62331b4e5ba300e2cf3e9d43f0e1ea065e52a5dc
bimaadam <rinbim.dev@gmail.com>
2025-06-02 17:52:18 +0700

    add metadata

--- src/app/(admin)/(transaction)/finance/income/page.tsx
+++ src/app/(admin)/(transaction)/finance/income/page.tsx
@@ -1,5 +1,11 @@
 import PageBreadcrumb from "@/components/common/PageBreadCrumb";
 import Transaction from "@/components/transaction";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "Transaksi | Admin Abyzain Jaya Teknika",
+    description: "Menu Transaksi"
+}
 
 export default function FinancePageIncome() {
     return (
--- src/middleware.ts
+++ src/middleware.ts
@@ -3,16 +3,13 @@ import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
 export function middleware(request: NextRequest) {
-  // Middleware tidak bisa akses localStorage langsung
-  // Kita akan menggunakan cookie sebagai alternatif
   const token = request.cookies.get('accessToken')?.value;
   const { pathname } = request.nextUrl;
 
-  // Daftar route yang diproteksi
-  const protectedRoutes = ['/'];
+  const protectionroute = ['/'];
 
   
-  if (protectedRoutes.some(route => pathname.startsWith(route))) {
+  if (protectionroute.some(route => pathname.startsWith(route))) {
     if (!token) {
       const url = request.nextUrl.clone();
       url.pathname = '/signin';
@@ -24,5 +21,5 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/', '/dashboard/:path*', '/home/:path*'],
+  matcher: ['/', '/home/:path*'],
 };
