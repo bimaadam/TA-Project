0da55658afb095440f66cbe0637f262e13bacb53
bimaadam <bimaadam.dev@gmail.com>
2025-09-12 22:50:34 +0700

    fix interface

--- src/app/(admin)/data-invoice/details-invoice/[id]/page.tsx
+++ src/app/(admin)/data-invoice/details-invoice/[id]/page.tsx
@@ -1,9 +1,16 @@
 import PageBreadcrumb from "@/components/common/PageBreadCrumb";
 import PreviewInvoice from "@/components/data-invoice/PreviewInvoice";
 
-export default function DetailInvoice({ params }: { params: { id: string } }) {
+interface DetailInvoicePageProps {
+    params: Promise<{ id: string }>;
+}
+
+export default async function DetailInvoice({
+    params,
+}: DetailInvoicePageProps) {
+    const { id } = await params;
     return <div>
         <PageBreadcrumb pageTitle="Invoice Details" />
-        <PreviewInvoice invoiceId={params.id} />
+        <PreviewInvoice invoiceId={id} />
     </div>
 }
