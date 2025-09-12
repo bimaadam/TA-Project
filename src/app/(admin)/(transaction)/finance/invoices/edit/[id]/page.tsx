3f113437c289d932f577d2ae3bb85aa332ba28b5
bimaadam <bimaadam.dev@gmail.com>
2025-09-12 22:22:03 +0700

    fucek tsx

--- src/app/(admin)/(transaction)/finance/invoices/edit/[id]/page.tsx
+++ src/app/(admin)/(transaction)/finance/invoices/edit/[id]/page.tsx
@@ -2,15 +2,12 @@ import EditInvoiceForm from "@/components/invoice/EditInvoiceForm";
 import ComponentCard from "@/components/common/ComponentCard";
 import PageBreadcrumb from "@/components/common/PageBreadCrumb";
 
-interface PageProps {
-  params: {
-    id: string;
-  };
-}
-
-export default async function EditInvoicePage({ params }: PageProps) {
-  const { id } = params;
-
+export default async function EditInvoicePage({
+  params,
+}: {
+  params: { id: string };
+}) {
+    const { id } = params;
   return (
     <div>
       <PageBreadcrumb pageTitle="Edit Faktur" />
