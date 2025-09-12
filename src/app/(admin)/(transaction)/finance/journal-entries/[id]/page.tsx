8e08275b6fd3b292b94b40ddb70020b98aefe72a
bimaadam <bimaadam.dev@gmail.com>
2025-09-12 22:33:23 +0700

    fix inline type

--- src/app/(admin)/(transaction)/finance/journal-entries/[id]/page.tsx
+++ src/app/(admin)/(transaction)/finance/journal-entries/[id]/page.tsx
@@ -2,8 +2,12 @@ import ViewJournalEntry from "@/components/journal/ViewJournalEntry";
 import ComponentCard from "@/components/common/ComponentCard";
 import PageBreadcrumb from "@/components/common/PageBreadCrumb";
 
-export default async function ViewJournalEntryPage({ params }: { params: { id: string } }) {
-    const {id} = params
+export default async function ViewJournalEntryPage({
+    params,
+}: {
+        params: Promise<{ id: string }>;
+    }) {
+    const {id} = await params
     return (
         <div>
             <PageBreadcrumb pageTitle="Detail Entri Jurnal" />
