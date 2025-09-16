c6eb50971d3e9c6bddc7044bca21758085346d8a
bimaadam <bimaadam.dev@gmail.com>
2025-09-16 23:41:15 +0700

    middleware not assign

--- src/middleware.ts
+++ src/middleware.ts
@@ -21,5 +21,5 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+  matcher: ["/", "/signin", "/signup", "/reset-pwd"],
 };
